798a819e0b7bbae0b3f64467d5816cea0a04ec99
sergey <sapokrovsky@gmail.com>
2023-03-07 10:26:41 +0300

    fix: 1 review

--- src/Component/SavedMovies/MoviesCard/MoviesCard.js
+++ src/Component/SavedMovies/MoviesCard/MoviesCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import card from "../../../images/card.png"
+import icon from "../../../images/icon.svg"
 
 function MoviesCard() {
+  const [cardAcive, setCardAcive] = useState(false);
     return (
-          <li className="card">
+          <li className="card" onClick={()=> setCardAcive(!cardAcive)}>
+            <img className={`card__icon ${cardAcive ? 'active' : ''}`} src={icon} alt="Иконка" />
             <img className="card__image" src={card} alt="Карточка"/>
             <div className="card__info">
               <p className="card__name">33 слова о дизайне</p>

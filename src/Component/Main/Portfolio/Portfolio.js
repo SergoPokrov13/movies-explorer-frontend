3c58468e0bfbcf034667489f52bf48be9d4f9ad6
sergey <sapokrovsky@gmail.com>
2023-03-15 23:58:58 +0300

    fix profile

--- src/Component/Main/Portfolio/Portfolio.js
+++ src/Component/Main/Portfolio/Portfolio.js
@@ -5,11 +5,11 @@ function Portfolio() {
     return (
             <section className="portfolio">
                 <p className="portfolio__title">Портфолио</p>
-                <a href="https://github.com/SergoPokrov13/how-to-learn" target="_blank" className="portfolio__web button">
+                <a href="https://sergopokrov13.github.io/how-to-learn/" target="_blank" className="portfolio__web button">
                     <p className="portfolio__web_name">Статичный сайт</p>
                     <img className="portfolio__web_link" src={link} alt=""/>
                 </a>
-                <a href="https://github.com/SergoPokrov13/russian-travel" target="_blank"  className="portfolio__web button">
+                <a href="https://sergopokrov13.github.io/russian-travel/" target="_blank"  className="portfolio__web button">
                     <p className="portfolio__web_name">Адаптивный сайт</p>
                     <img className="portfolio__web_link" src={link} alt=""/>
                 </a>

bbc88c987390227ac48ea0ac2db8cf72bcba4f2f
sergey <sapokrovsky@gmail.com>
2023-03-19 19:22:44 +0300

    проверка на сервере

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -99,7 +99,7 @@ class MainApi {
   }
   
   const mainApi = new MainApi({
-    url: "http://localhost:3002/api",
+    url: "https://sergey.diplom.work.nomoredomains.work/api",
     headers: {
       "Content-Type": "application/json",
     }
